588ab05dc6a3ece6861660001e56c8b0efe5a44b
Tiamoyk <2955639097@qq.com>
2017-10-13 13:07:49 +0800

    down

---
+++ index.js
@@ -0,0 +1,10 @@
+var box = document.getElementById('box');
+
+box.addEventListener('mousedown',down)
+
+function down(ev){
+	
+	var disX = ev.clientX-this.offsetLeft;
+	var disY = ev.clientY.offsetTop;
+	
+}
